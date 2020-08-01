27b3019c1f45b92b75ad64cd37e0ad53cd91cb89
Srinath <srinathbasker@gmail.com>
2020-08-01 21:50:17 +0530

    added basic parallax feature

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -10,10 +10,26 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import PlainCard from '../components/cards/PlainCard';
 import { justAClickAway, homeHeroCard } from '../mockData/cardData';
 import { ParallaxCard } from '../components/cards';
+import HomeImage from './../assets/home_port_1.jpg';
+
 const styles = StyleSheet.create({
   container: style.container,
   content: {
     backgroundColor: "#fff"
+  },
+  title: {
+    position: "absolute",
+    bottom: 200,
+    right: 0,
+    left: 0,
+    display: "flex",
+    textAlign: "center",
+    color: "#fff",
+    marginHorizontal: 'auto',
+    fontSize: 30,
+    padding: 30,
+    lineHeight: 60,
+    zIndex: 10
   }
 });
 
@@ -22,13 +38,36 @@ export default function Home({ navigation }) {
   console.log(justAClickAway)
   return (
     <>
-      <Header toggleDrawer={navigation.toggleDrawer} />
-      <ScrollView >
+      {/* <Header toggleDrawer={navigation.toggleDrawer} /> */}
+      {/* <ScrollView >
         <View style={styles.content}>
           <ParallaxCard {...homeHeroCard} />
           <PlainCard {...justAClickAway} />
         </View>
-      </ScrollView >
+      </ScrollView > */}
+      <ParallaxScrollView
+        backgroundColor="transparent"
+        foregroundClolor="white"
+        backgroundScrollSpeed={5}
+        parallaxHeaderHeight={650}
+        renderForeground={() => (
+          <>
+            <Text style={styles.title} className="cardTitle">FLY{"\n"}WHENEVER,{"\n"}WHEREVER </Text>
+          </>
+        )}
+        renderFixedHeader={() => <Header />}
+        fadeOutForeground={false}
+        renderBackground={() => (
+          <Image style={{ display: 'flex', width: '100%', height: '100%' }} resizeMode="cover" source={HomeImage} />
+        )}>
+        <PlainCard {...justAClickAway} />
+      
+      </ParallaxScrollView>
+      
+      {/* <ScrollView >
+
+      </ScrollView> */}
+
     </>
   );
 }
